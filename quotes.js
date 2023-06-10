a9f1ffcadc8a80e853b16da427c9f988e92e8950
Dylan Williams <williamsdylanhello@gmail.com>
2023-06-09 23:19:45 -0700

    big change

---
+++ quotes.js
@@ -0,0 +1,18 @@
+const localQuotes = [
+    {
+        text: '1',
+        author: 'a',
+    },
+    {
+        text: '2',
+        author: 'b',
+    },
+    {
+        text: '3',
+        author: 'c',
+    },
+    {
+        text: '4',
+        author: 'd',
+    },
+]
